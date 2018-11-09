f4ce1f1d52d2c5ebc43fe8ebc12a9915cd9ff92c
Aurel Pintea <winofame@gmail.com>
2018-11-09 11:47:07 +0100

    Initial commit for the 'RPC (Remote Procedure Call)' application.

---
+++ src/main/java/com/example/demo/EmployeeNotFoundException.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+@SuppressWarnings("serial")
+class EmployeeNotFoundException extends RuntimeException {
+
+    EmployeeNotFoundException(Long id) {
+	super("Could not find employee " + id);
+    }
+}
---
+++ src/main/java/com/example/demo/RestFromZeroApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestFromZeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestFromZeroApplication.class, args);
+	}
+}
